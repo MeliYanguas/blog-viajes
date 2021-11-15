6fc1980f93d0ced8de0d47ac93672161cc9a99c3
Meli Yanguas <melinayanguas@gmail.com>
2021-11-15 13:40:21 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'ejs');
+
+// middlewares
+
+//routes
+app.get('/', (req, res) => {
+    res.render('index.html', { title: 'Viajes' });
+})
+
+//static files
+app.use(express.static(path.join(__dirname,'public')));
+
+// listening the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'))
+})
